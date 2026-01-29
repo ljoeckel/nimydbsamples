2109c7a09d3c94716e6094abe2d541e9906be240
ljoeckel <lothar.joeckel@gmail.com>
2026-01-29 13:35:09 +0000

    compacted

--- src/datastar/form.nim
+++ src/datastar/form.nim
@@ -1,102 +1,89 @@
-## Run with: nim c -r examples/form.nim
+## Run in the src/datastar directory: nim c -r form.nim
 ## Then open http://localhost:8080 in your browser
 
-import std/[asyncdispatch, asynchttpserver, json, strutils, strformat, times]
+import std/[asyncdispatch, asynchttpserver, times, json, strutils]
 import std/[posix]
 import datastar
 import datastar/asynchttpserver as DATASTAR
 
 const HTML = "html"
 
+
 # Shutdown
 proc handleSignal() {.noconv.} =
-  echo "\nShutting down..."
-  quit(0)
+    echo "\nShutting down..."
+    quit(0)
 setControlCHook(handleSignal)
+
 
 #-------------
 # Handler's
 #-------------
+proc handleStatic(req: Request, url: string, content: string) {.async.} =
+    let indexHtml = readFile(HTML & "/" & url)
+    await req.respond(Http200, indexHtml, newHttpHeaders([("Content-Type", content)]))
 
-# Load /
-proc handleIndex(req: Request) {.async.} =
-    let indexHtml = readFile(HTML & "/form.html")
-    await req.respond(Http200, indexHtml, newHttpHeaders([("Content-Type", "text/html")]))
 
-# Load /style.css
-proc handleStyle(req: Request) {.async.} =
-    let css = readFile(HTML & "/style.css") 
-    await req.respond(Http200, css, newHttpHeaders([("Content-Type", "text/css")]))
+# Reload /
+proc reload(req: Request) {.async.} =
+    let sse = await req.newSSEGenerator(); defer: req.closeSSE()
+    await sse.executeScript("window.location.reload()")
 
-# Load /style.css
-proc handleIco(req: Request) {.async.} =
-  let css = readFile(HTML & "/favicon.ico") 
-  await req.respond(Http200, css, newHttpHeaders([("Content-Type", "image/x-icon")]))
 
-# Deliver /contact-sales
-proc handleContactSales(req: Request) {.async.} =
-  let indexHtml = readFile(HTML & "/sales.html")
-  await req.respond(Http200, indexHtml, newHttpHeaders([("Content-Type", "text/html")]))
+# Validate E-Mail
+proc handleValidateEmail(req: Request) {.async.} =
+    let signals = parseJson(req.body)
+    let email = signals["email"].getStr()
+    let isInvalid = email.len > 0 and not email.contains("@")
+    let sse = await req.newSSEGenerator(); defer: req.closeSSE()
+    await sse.patchSignals(%*{
+        "emailInvalid": isInvalid,
+        "canSubmit": not isInvalid
+    })
+
+
+# Handle Form submit
+proc handleSubmit(req: Request) {.async.} =
+    let signals = parseJson(req.body)
+    let name = signals["name"].getStr()
+    let email = signals["email"].getStr()
+
+    let msg = if name.len > 0 and email.len > 0:
+            "<div id='response-message' class='formsuccess'>Thank you '" & name & "', Data received!</div>"
+        else:
+            "<div id='response-message' class='formerror'>Sorry! Invalid or missing data!</div>"
+
+    let sse = await req.newSSEGenerator(); defer: req.closeSSE()
+    await sse.patchElements(msg)
+
+    # Show all signals in the Message Textfield
+    await sse.patchSignals(%*{"message": $signals})
+
 
 # Send the servertime to the client each second
 proc handleServerEvents(req: Request) {.async.} =
     # connection is opened with <head data-init="@get('/server-events')">
     let sse = await req.newSSEGenerator(); defer: req.closeSSE()
     while true:
-        let msg = fmt"<div id='events-message' class='formsuccess'>" & $now() & "</div>"
+        let msg = "<div id='events-message' class='formsuccess'>" & $now() & "</div>"
         await sse.patchElements(msg)
         await sleepAsync(1000)
 
-# Reload /
-proc reload(req: Request) {.async.} =
-  let sse = await req.newSSEGenerator(); defer: req.closeSSE()
-  await sse.executeScript("window.location.reload()")
 
-# Validate E-Mail
-proc handleValidateEmail(req: Request) {.async.} =
-  let signals = parseJson(req.body)
-  let email = signals["email"].getStr()
-  let isInvalid = email.len > 0 and not email.contains("@")
-  let sse = await req.newSSEGenerator(); defer: req.closeSSE()
-  await sse.patchSignals(%*{
-    "emailInvalid": isInvalid,
-    "canSubmit": not isInvalid
-  })
+proc router(req: Request) {.async.} =
+    case req.url.path
+    of "/": await handleStatic(req, "form.html", "text/html") #await handleIndex(req)
+    of "/favicon.ico": await handleStatic(req, "favicon.ico", "image/x-icon") #await handleIco(req) # deliver CSS
+    of "/style.css": await handleStatic(req, "style.css", "text/css") #await handleStyle(req) # deliver CSS
+    of "/contact-sales": await handleStatic(req, "sales.html", "text/html") #await handleContactSales(req) # redirect to sales department
+    of "/server-events": await handleServerEvents(req)
+    of "/validate-email": await handleValidateEmail(req) # validate email field
+    of "/submit-form": await handleSubmit(req) # receive formdata
+    else:
+        echo "NOT FOUND: req.url=", req.url
+        await req.respond(Http404, "Not Found")
 
-# Handle Form submit
-proc handleSubmit(req: Request) {.async.} =
-  let signals = parseJson(req.body)
-  let name = signals["name"].getStr()
-  let email = signals["email"].getStr()
-  let message = signals["message"].getStr()
-
-  var msg: string
-  if name.len > 0 and email.len > 0:
-    msg = fmt"<div id='response-message' class='formsuccess'>Thank you '{name}', Data received!</div>"
-  else:
-    msg = "<div id='response-message' class='formerror'>Sorry! Invalid or missing data!</div>"
-
-  let sse = await req.newSSEGenerator(); defer: req.closeSSE()
-  await sse.patchElements(msg)
-  # Show all signals in the Message Textfield
-  await sse.patchSignals(%*{"message": $signals})
-
-
-proc handler(req: Request) {.async.} =
-  case req.url.path
-  of "/": await handleIndex(req)
-  of "/server-events": await handleServerEvents(req)
-  of "/style.css": await handleStyle(req) # deliver CSS
-  of "/favicon.ico": await handleIco(req) # deliver CSS
-  of "/validate-email": await handleValidateEmail(req) # validate email field
-  of "/submit-form": await handleSubmit(req) # receive formdata
-  of "/contact-sales": await handleContactSales(req) # redirect to sales department
-  else:
-    await req.respond(Http404, "Not Found")
-
-proc main() =
-  let server = newAsyncHttpServer()
-  echo "Server running at http://localhost:8080 (Ctrl+C to stop)"
-  waitFor server.serve(Port(8080), handler)
-
-main()
+if isMainModule:
+    let server = newAsyncHttpServer()
+    echo "Server running at http://localhost:8080 (Ctrl+C to stop)"
+    waitFor server.serve(Port(8080), router)
